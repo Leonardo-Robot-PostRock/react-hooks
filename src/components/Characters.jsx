4c44cfbbd9b07e97675eca6f3039b1210e5b7a04
Leonardo Puebla <leonardo38361@gmail.com>
2022-04-27 17:47:53 -0300

    Se agregan estilos a cada card

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "../styles/Characters.css";
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
@@ -11,16 +12,23 @@ const Characters = () => {
 
   return (
     <div className="Characters">
-      {characters.map((character) => (
-        <div className="container">
-          <h2 className="item item-1">{character.name}</h2>
-          <img src={character.image} className="item item-6" alt="character"/>
-          <h3 className="item item-2">{character.status}</h3>
-          <h3 className="item item-3">{character.species}</h3>
-          <h3 className="item item-4">{character.type}</h3>
-          <h3 className="item item-5">{character.gender}</h3>
-          <h3 className="item item-6">{character.origin.name}</h3>
-          <h3 className="item item-6">{character.location.name}</h3>
+      {characters.map((character, index) => (
+        <div className="container" key={index} id={index}>
+          <div className="container-character">
+            <h2 className="item item-1">{character.name}</h2>
+            <img
+              src={character.image}
+              className="item item-6"
+              alt="character"
+            />
+          </div>
+          <div className="container-data">
+            <h3 className="item item-2">{character.status}</h3>
+            <h3 className="item item-3">{character.species}</h3>
+            <h3 className="item item-5">{character.gender}</h3>
+            <h3 className="item item-6">{character.origin.name}</h3>
+            <h3 className="item item-6">{character.location.name}</h3>
+          </div>
         </div>
       ))}
     </div>
